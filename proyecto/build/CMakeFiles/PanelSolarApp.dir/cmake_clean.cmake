0a6ea87e2af8f3aef0ca3c39dc64fb48b16b6e96
Alvaro Morales <aitom@ideapad1i.localdomain>
2026-03-27 21:39:16 -0500

    Creacion del ejecutable

---
+++ proyecto/build/CMakeFiles/PanelSolarApp.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PanelSolarApp.dir/src/PanelSolar.cpp.o"
+  "CMakeFiles/PanelSolarApp.dir/src/PanelSolar.cpp.o.d"
+  "CMakeFiles/PanelSolarApp.dir/src/main.cpp.o"
+  "CMakeFiles/PanelSolarApp.dir/src/main.cpp.o.d"
+  "PanelSolarApp"
+  "PanelSolarApp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PanelSolarApp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ proyecto/build/CMakeFiles/PanelSolarApp.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for PanelSolarApp.
+# This may be replaced when dependencies are built.
